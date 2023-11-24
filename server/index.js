d9e56cdc9c6792ed09b325694c2111c0ed822321
KomalPawar-29 <komalpawar20003@gmail.com>
2023-11-24 22:31:23 +0530

    added changes

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ dotenv.config();
 
 import mongoose from 'mongoose';
 import Product from './models/Product.js';
+import User from './models/User.js';
+import Order from './models/Order.js';
 
 const app = express();
 app.use(express.json());
@@ -21,21 +23,30 @@ app.post('/product', async (req,res)=>{
     // const name = req.body.name;
     // const price = req.body.price;
     // const description = req.body.description;
-    const {name, price, description } = req.body;
+    const {name, price, description, image } = req.body;
 
     const product = new Product({
         name:name,
         price: price,
-        description: description
+        description: description,
+        image: image
     });
 
-    const savedProduct = await product.save();
+    try{
+        const savedProduct = await product.save();
 
     res.json({
         success:true,
         data:product,
         message:'Product added successfully'
     })  
+    } catch (e) {
+        res.json({
+            success:false,
+            message:e.message
+        })  
+    }
+   
 })
 
 
@@ -75,12 +86,13 @@ app.delete("/product/:id", async (req, res)=> {
 
 app.put("/product/:id", async (req, res)=> {
     const {id} = req.params;
-    const {name, price, description} = req.body;
+    const {name, price, description, image} = req.body;
 
     await Product.updateOne({_id: id}, {$set: {
         name: name,
         price: price,
-        description: description
+        description: description,
+        image: image
     }});
      
     const updateProduct = await Product.findOne({_id: id});
@@ -92,8 +104,86 @@ app.put("/product/:id", async (req, res)=> {
     })
 });
 
+app.post("/signup", async(req, res)=>{
+    const {name, email, mobile, password} = req.body;
+
+    const user = new User({
+        name: name,
+        email: email,
+        mobile: mobile,
+        password: password
+    });
+
+    try{
+        const savedUser = await user.save();
+
+        return res.json({
+            success: true,
+            data: savedUser,
+            message: "User registered successfully"
+        })
+    }
+    catch(e){
+        return res.json({
+            success: false,
+            message: e.message
+    })
+}
+})
+
+app.post("/login", async(req, res)=>{
+    const {email, password} = req.body;
+
+    const user = await User.findOne({email: email, password: password});
+
+    if(user){
+        return res.json({
+            success: true,
+            data: user,
+            message:"User logged in successfully"
+        })
+    }
+    else
+    {
+        return res.json({
+            success: false,
+            message: "Invalid email or password"
+        })
+    }
+})
+app.post("/order", async(req, res)=>{
+    const {product, user, quantity, shippingAddress} = req.body;
+  
+    const order = new Order({
+      product: product,
+      user: user,
+      quantity: quantity,
+      shippingAddress: shippingAddress
+    });
+  
+    const savedOrder = await order.save();
+  
+    return res.json({
+      success: true,
+      data: savedOrder,
+      message: "Order placed successfully"
+    })
+  })
+
+app.get("/orders", async(req, res)=>{
+    const {id} = req.query;
+
+    const orders = await Order.find({user:id}).populate("product user");
+
+    res.json({
+        success:true,
+        data: orders,
+        message: "Orders retrieved successfully"
+    })
+})
+
 const PORT =5000;
 
 app.listen(PORT, ()=>{
-    console.log('Server is running on port ${PORT}');
+    console.log(`Server is running on port ${PORT}`);
 });
