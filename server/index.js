b5c276b8ca06500e76d8656013c675c2c02a0f83
KomalPawar-29 <komalpawar20003@gmail.com>
2023-11-25 20:11:35 +0530

    Final Changes

--- server/index.js
+++ server/index.js
@@ -195,5 +195,5 @@ if(process.env.NODE_ENV === "production"){
 const PORT =5000;
 
 app.listen(PORT, ()=>{
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on  ${PORT}`);
 });
